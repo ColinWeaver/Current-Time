8c43f67f4a8a0111e6c299b60fa279e62cc02bf5
ColinWeaver <81534398+ColinWeaver@users.noreply.github.com>
2022-03-20 21:39:51 -0500

    first commit

---
+++ src/app.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const request = require('supertest');
+const morgan = require('morgan');
+const path = require('path');
+const fs = require('fs');
+const app = express();
+
+//I used morgan logging: https://expressjs.com/en/resources/middleware/morgan.html
+app.use(morgan('tiny', {
+  stream: fs.createWriteStream(path.join(__dirname, './API.log'))
+}))
+
+
+//GET Route
+app.get("/whattimeisit", (req, res) => {
+  
+const timeObject = new Date();
+const militaryHour = timeObject.getHours();
+const minutes = timeObject.getMinutes();
+let currentTime;
+
+if (militaryHour > 12) {
+  currentTime = `${militaryHour - 12}:${minutes} PM`
+}
+else {
+  currentTime =  `${militaryHour}:${minutes} AM`
+}
+
+res.send(currentTime);
+});
+
+
+//I used the supertest module for this: https://www.npmjs.com/package/supertest
+request(app)
+  .get('/whattimeisit').expect(200)
+  .end((err) => {
+    if (err) throw err;
+  });
+
+
+
+
+module.exports = app;
